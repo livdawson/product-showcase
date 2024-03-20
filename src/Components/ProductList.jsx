f05cc520f5ce054b4e6b7527d7b76a8301ec435d
livdawson <olivia.dawson@btinternet.com>
2024-03-20 14:40:45 +0000

    only render 10 random products onto page and order by rating descending

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,58 +1,59 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { getAllProducts } from "../Utils/api";
+import { selectRandomProducts } from "../Utils/utils";
 import ErrorComponent from "./ErrorComponent";
 import ProductCard from "./ProductCard";
 
 export default function ProductList() {
-    const [products, setProducts] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const products = await getAllProducts();
-            const filteredProducts = products.filter(product => product.brand !== "Apple");
-            setProducts(filteredProducts);
-            setIsLoading(false);
-          } catch (err) {
-            setIsLoading(false);
-            setError("Error fetching products: " + err.message);
-          }
-        };
-      
-        fetchData();
-      }, []);
-
-      if (error) {
-        return (
-          <ErrorComponent
-            message={error}
-          />
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const products = await getAllProducts();
+        const filteredProducts = products.filter(
+          (product) => product.brand !== "Apple"
         );
-      } else {
-        return (
-            <main>
-            {isLoading ? (
-              <p>Fetching products...</p>
-            ) : (
-                <div>
-                {products.map((product) => (
-                  <ProductCard
-                    key={product.id}
-                    productID={product.id}
-                    title={product.title}
-                    description={product.description}
-                    price={product.price}
-                    discountPercentage={product.discountPercentage}
-                    rating={product.rating}
-                    stock={product.stock}
-                    image={product.images[0]}
-                  />
-                ))}
-              </div>
-            )}
-            </main>
-    )
-    }
+        const selectedProducts = selectRandomProducts(filteredProducts, 10);
+        selectedProducts.sort((a, b) => b.rating - a.rating);
+        setProducts(selectedProducts);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        setError("Error fetching products: " + err.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <ErrorComponent message={error} />;
+  } else {
+    return (
+      <main>
+        {isLoading ? (
+          <p>Fetching products...</p>
+        ) : (
+          <div>
+            {products.map((product) => (
+              <ProductCard
+                key={product.id}
+                productID={product.id}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                discountPercentage={product.discountPercentage}
+                rating={product.rating}
+                stock={product.stock}
+                image={product.images[0]}
+              />
+            ))}
+          </div>
+        )}
+      </main>
+    );
+  }
 }
