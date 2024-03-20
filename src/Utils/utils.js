01b6cdfc975da97d32d9abd48c431437d348fe4b
livdawson <olivia.dawson@btinternet.com>
2024-03-20 16:10:15 +0000

    create utility function to format countdown timer

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -3,3 +3,9 @@ export function selectRandomProducts(products, count) {
     return shuffledProducts.slice(0, count);
 };
 
+export function formatTime(time) {
+    const hours = Math.max(0, Math.floor(time / 3600)); 
+    const minutes = Math.max(0, Math.floor((time % 3600) / 60));
+    const seconds = Math.max(0, Math.floor(time % 60));
+    return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+};
